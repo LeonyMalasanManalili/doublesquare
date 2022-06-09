ad0f81df32c2c9ba83ad6a5a170990935e65790c
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-09 02:27:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Paginaequadrati() {
+  return (
+    <div className="App">
+          <div className='red'>Rosso-dalla-Rabbia</div>
+    </div>
+  );
+}
+
+export default Paginaequadrati;
